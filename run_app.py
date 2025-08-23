385d22ca3daaa296a1b2747079b545a1c1efeace
Mohamed Marouk <mohamedmarouk55@gmail.com>
2025-08-23 14:57:54 +0300

    🚀 كود نهائي: دعم كامل لـ Supabase

--- run_app.py
+++ run_app.py
@@ -16,7 +16,7 @@ import threading
 import time
 
 # استيراد Flask وSQLAlchemy
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 # استيراد Supabase للتخزين
